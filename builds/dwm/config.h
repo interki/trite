28b8c556111356afc42971d8e12039aac5db1e76
interki <tom_is@hotmail.com>
2020-04-21 18:08:08 +1000

    up

--- builds/dwm/config.h
+++ builds/dwm/config.h
@@ -12,7 +12,7 @@ static const char *fonts[]          = { "Terminus (TTF):pixelsize=16" };
 // static const char dmenufont[]       = "xos4 Terminus:pixelsize=16:antialias=true:autohint=true";
 static const char dmenufont[]       = "Terminus (TTF):pixelsize=16";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444"; 
+static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 // static const char col_cyan[]        = "#005577";
